cfd88c7c82e0a8d397b3ec6940dcda9e4226a6c4
shumailakhan4810 <shumaila_sohaib@aptechnorth.edu.pk>
2024-08-16 10:49:53 +0500

    add newfile

---
+++ app.js
@@ -0,0 +1,65 @@
+for(i=1; i<products.length; i++){
+    document.getElementById('select1'). innerHTML += `
+    <option value="${i}">${products[i].title}</option>
+    `
+}
+
+for(i=1; i<products.length; i++){
+    document.getElementById('select2'). innerHTML += `
+    <option value="${i}">${products[i].title}</option>
+    `
+}
+// 1st method
+function item1(a){
+    document.getElementById('img1').src=products[a].image;
+    document.getElementById('brand1').innerHTML=products[a].brand;
+    document.getElementById('price1').innerHTML=products[a].price;
+    document.querySelector('#desc1').innerHTML=products[a].description;
+
+
+}
+
+function item2(a){
+    document.getElementById('img2').src=products[a].image;
+    document.getElementById('brand2').innerHTML=products[a].brand;
+    document.getElementById('price2').innerHTML=products[a].price;
+    document.querySelector('.desc2').innerHTML=products[a].description;
+
+
+}
+
+
+
+
+// 2nd method
+function item1(a){
+    let select2=document.getElementById('select2').value;
+    if(a!=select2){
+    document.getElementById('img1').src=products[a].image;
+    document.getElementById('brand1').innerHTML=products[a].brand;
+    document.getElementById('price1').innerHTML=products[a].price;
+    document.querySelector('#desc1').innerHTML=products[a].description;
+}
+else{
+    document.getElementById('img1').src=products[0].image;
+    document.getElementById('brand1').innerHTML=products[0].brand;
+    document.getElementById('price1').innerHTML=products[0].price;
+    document.querySelector('#desc1').innerHTML=products[0].description;
+}
+}
+function item2(a){
+
+    let select1=document.getElementById('select1').value;
+    if(a!=select1){
+    document.getElementById('img2').src=products[a].image;
+    document.getElementById('brand2').innerHTML=products[a].brand;
+    document.getElementById('price2').innerHTML=products[a].price;
+    document.querySelector('.desc2').innerHTML=products[a].description;
+}
+else{
+    document.getElementById('img2').src=products[0].image;
+    document.getElementById('brand2').innerHTML=products[0].brand;
+    document.getElementById('price2').innerHTML=products[0].price;
+    document.querySelector('.desc2').innerHTML=products[0].description;
+}
+}
